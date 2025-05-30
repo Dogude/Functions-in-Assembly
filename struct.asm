a385f5dbda0a1eb304fa679ed2523806a82f166f
dogude <131747396+Dogude@users.noreply.github.com>
2025-05-30 22:12:38 +0300

    Update struct.asm

--- struct.asm
+++ struct.asm
@@ -4,11 +4,11 @@
   	lea    rax,[rbp-0x8]
    	mov    QWORD PTR [rbp-0x10],rax
    	mov    rax,QWORD PTR [rbp-0x10]
-   	add    rax,0x4
-   	mov    ecx,DWORD PTR [rax]
+   	add    rax,0x4                     ; jump 4 byte
+   	mov    ecx,DWORD PTR [rax]         ; get int content
   	mov    rdx,rcx
-  	add    ecx,0x1
-   	mov    DWORD PTR [rax],ecx
+  	add    ecx,0x1                     ; increment it
+   	mov    DWORD PTR [rax],ecx         ; then restore it
    	mov    eax,0x0
    	leave
    	ret
